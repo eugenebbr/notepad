6c0f8bc7cce9b1fbf4d0d76abb390ded0839c860
eugene.bbr <eugene.bbr@gmail.com>
2017-10-05 13:09:29 +0300

    Notepad v1, working

--- task.rb
+++ task.rb
@@ -1,3 +1,7 @@
+require_relative 'post.rb'
+
+require 'date'
+
 class Task < Post
   def initialize
     super
@@ -5,11 +9,19 @@ class Task < Post
   end
 
   def read_from_console
+    puts "Что надо сделать?"
+    @text = STDIN.gets.chomp
 
+    puts "К какому числу нужно сделать? Введите в формате ДД.ММ.ГГГГ (Например, 12.13.2014)"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
-
+    time_string = "Создано #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+    dead_line = "Крайний срок #{@due_date}"
+    return [dead_line, @text, time_string]
   end
 
 end
